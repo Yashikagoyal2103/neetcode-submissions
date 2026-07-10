cc33550d9a08449cada04d176d92632507260b77
Yashikagoyal2103 <161195786+Yashikagoyal2103@users.noreply.github.com>
2026-07-10 22:08:07 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hash= new HashMap<>();
+        for(int i=0; i<nums.length;i++){
+            hash.put(nums[i], hash.getOrDefault(nums[i], 0) +1);
+            if(hash.get(nums[i]) > 1){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
